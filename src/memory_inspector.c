582ca3ed401f5ed55f0bda728bb972c89f149dc0
Thomas Witte <t-witte@web.de>
2015-08-30 18:00:53 +0200

    Added License

--- src/memory_inspector.c
+++ src/memory_inspector.c
@@ -1,3 +1,22 @@
+/*
+    This file is part of dynasmgb.
+
+    dynasmgb is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    dynasmgb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with dynasmgb.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright 2014, 2015 Thomas Witte
+*/
+
 #include "memory_inspector.h"
 
 void draw_tile(memory_inspector_t *inspector, uint16_t base_addr, int pos_x, int pos_y, int zoom) {
